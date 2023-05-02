061bfcb28c8cadcb21046c6d4c09b3c7c98eed3a
teyik0 <teyik0.fr@icloud.com>
2023-05-02 21:14:40 +0200

    contract

--- contract/contracts/Pmu.sol
+++ contract/contracts/Pmu.sol
@@ -1,26 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 
-contract Pmu {
-    string public name = "PMU";
-    uint256 public pmuCount = 0;
+contract Pmu is VRFV2WrapperConsumerBase {
+    address constant linkAddress = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+    address constant vrfWrapperAddress =
+        0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+
+    constructor() VRFV2WrapperConsumerBase(vrfWrapperAddress, linkAddress) {}
+
+    uint256 public pmuGameCount = 0;
 
     struct Horse {
         uint256 id;
         uint256 step;
     }
 
-    string public message;
+    Horse[] public horses;
 
-    constructor(string memory initMessage) {
-        message = initMessage;
+    function initialize() private {
+        for (uint i = 0; i < 6; i++) {
+            horses[i] = Horse(i, 0);
+        }
+        pmuGameCount++;
     }
 
-    // A public function that accepts a string argument and updates the `message` storage variable.
-    function update(string memory newMessage) public {
-        message = newMessage;
+    function participate() public {
+        uint256 vrfSeed = uint256(keccak256(abi.encodePacked(msg.sender)));
+    }
+
+    function goForward(uint _id) public {
+        Horse memory _horse = horses[_id];
+        _horse.step += 1;
+        horses[_id] = _horse;
     }
 }
